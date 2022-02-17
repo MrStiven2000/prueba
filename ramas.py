67b9c6396155dbb5547765254a9daf8ce0af0334
stiven@gmail.com <julyrodriguez502afa>
2022-02-17 14:33:35 -0500

    hola

--- ramas.py
+++ ramas.py
@@ -18,4 +18,19 @@ if SensorDeAgua ==("normal"):
 if SensorDeAgua==("mayor"):
     print("error, el agua consumible de los pollos es de 300 litros")
 if SensorDeAgua <=("bajo"):
-    print("alerta, el agua de los pollos es muy poca, es de 100 litros")
+    print("alerta, el agua de los pollos es baja, es de 100 litros")
+# sensor de luz 
+if SensorFotoResistencia ==("dia"):
+   print("la puerta se ha abierto ya se hizo de dia")
+if SensorFotoResistencia ==("noche"):
+   print("las puertas se ceraron, los pollos estan adentro")
+# sensor de movimientos hostiles
+if SensorDeMovimiento ==("detectando"):
+   print("se detecta movimiento de un gato, metiendo a los pollos")
+if SensorDeMovimiento ==("sin detectar"):
+   print("no se detectan movimientos de otros animales")
+# sensor de calefaccion
+if Resistencia ==("0") and SensorFotoResistencia==("dia"):
+    print("la temperatura esta apagada y la puerta esta abierta")
+if Resistencia ==("1") and SensorFotoResistencia==("noche"):
+   print("la temepratura esta encendida y las puertas se cerraron")
